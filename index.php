54056201a7711956cf93fe918ce2dd11e9a4cd9b
chaynan <chaynan70@hotmail.com>
2018-08-06 11:09:12 +0700

    a

--- index.php
+++ index.php
@@ -21,10 +21,11 @@ if (!is_null($events['events'])) {
         // Get replyToken 
                 $replyToken = $event['replyToken']; 
         //Split message then keep it in database.
+        $appointments=explode(',', $event['message']['text']);
         if(count($appointments==2))
-        $host =
-        $dbname = 
-        $pass = 
+        $host ='ec2-23-23-242-163.compute-1.amazonaws.com';
+        $dbname ='dfitqn78lbn0av'; 
+        $pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
         $connection =new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
         $params=array(
             'time' => $appointments[0], 
