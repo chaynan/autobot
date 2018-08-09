75222bb2eb85642c664efb9a7ca14701a51e164d
chaynan <chaynan70@hotmail.com>
2018-08-09 16:39:53 +0700

    y

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $user = 'gwuaimhybkhmyz';
 $pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
 $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
 
-$sql = sprintf("SELECT * FROM poll");
+$sql = sprintf("SELECT * FROM poll WHERE id = 7");
 $result = $connection->query($sql); 
 if($result !== null) { 
     echo $result->rowCount(); 
