66fdd6e2b805f76c3b0c1e173d0260f3319b0197
chaynan <chaynan70@hotmail.com>
2018-08-09 10:47:01 +0700

    y

--- index.php
+++ index.php
@@ -1,29 +1,110 @@
 <?php
- 
 require_once('./vendor/autoload.php');
-
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-
 // Token
-$channel_token = 'JbkbMF0zqa9cDn91X8Vqhx0CMgD7haLJlO2V2bm8GpU4RZOZSvQHKw2stIMaFPN/Nthz2ZuAUdT7D3g2xUPcS4dvFtzF32s+C7zKtq+/hBR6VNIYXADNVgap6/7hMe46fUUW88Fm9JkRGbhdljSIvQdB04t89/1O/w1cDnyilFU=';
-$channel_secret = 'fce33b682da751c51e13169d81c9b7a8';
-
+$channel_token = '2MCOyCeaBipmw3ZzJG8BrsiO4KzCoaoPddMgbZtEu5HHVeIaWU+PDKcCZRJEY76zqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLrSkLdciLCuKUgV6aFrvAAuuG1mMWe7DCzfEW9FfHQhJR4F/m0AdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'd4afd7da941ac195c155fe67dcb5a338';
 // Create bot
 $httpClient = new CurlHTTPClient($channel_token);
 $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-
 // Database connection 
-$host = 'ec2-23-23-242-163.compute-1.amazonaws.com';
-$dbname = 'dfitqn78lbn0av';
-$user = 'gwuaimhybkhmyz';
-$pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
+$host = 'ec2-174-129-223-193.compute-1.amazonaws.com';
+$dbname = 'd74bjtc28mea5m';
+$user = 'eozuwfnzmgflmu';
+$pass = '2340614a293db8e8a8c02753cd5932cdee45ab90bfcc19d0d306754984cbece1';
 $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
-$result = $connection->query("SELECT * FROM .appointments");
-if($result !== null) {
-    echo $result->rowCount();
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+    
+        // Line API send a lot of event type, we interested in message only.
+		if ($event['type'] == 'message') {
+            switch($event['message']['type']) {
+                case 'text':
+                    
+                    $sql = sprintf(
+                        "SELECT * FROM slips WHERE slip_date='%s' AND user_id='%s' ", 
+                        date('Y-m-d'),
+                        $event['source']['userId']);
+                    $result = $connection->query($sql);
+                    if($result !== false && $result->rowCount() >0) {
+                        // Save database
+                        $params = array(
+                            'name' => $event['message']['text'],
+                            'slip_date' => date('Y-m-d'),
+                            'user_id' => $event['source']['userId'],
+                        );
+                        $statement = $connection->prepare('UPDATE slips SET name=:name WHERE slip_date=:slip_date AND user_id=:user_id'); 
+                        $statement->execute($params);
+                    } else {
+                        $params = array(
+                            'user_id' => $event['source']['userId'] ,
+                            'slip_date' => date('Y-m-d'),
+                            'name' => $event['message']['text'],
+                        );
+                        $statement = $connection->prepare('INSERT INTO slips (user_id, slip_date, name) VALUES (:user_id, :slip_date, :name)');
+                         
+                        $effect = $statement->execute($params);
+                    }
+                    // Bot response 
+                    $respMessage = 'Your data has saved.';
+                    $replyToken = $event['replyToken'];
+                    $textMessageBuilder = new TextMessageBuilder($respMessage);
+                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                    break;
+                case 'image':
+                    // Get file content.
+                    $fileID = $event['message']['id'];
+                    
+                    $response = $bot->getMessageContent($fileID);
+                    $fileName = md5(date('Y-m-d')).'.jpg';
+                    
+                    if ($response->isSucceeded()) {
+                        // Create file.
+                        $file = fopen($fileName, 'w');
+                        fwrite($file, $response->getRawBody());
+                        $sql = sprintf(
+                                    "SELECT * FROM slips WHERE slip_date='%s' AND user_id='%s' ", 
+                                    date('Y-m-d'),
+                                    $event['source']['userId']);
+                        $result = $connection->query($sql);
+                        if($result !== false && $result->rowCount() >0) {
+                            // Save database
+                            $params = array(
+                                'image' => $fileName,
+                                'slip_date' => date('Y-m-d'),
+                                'user_id' => $event['source']['userId'],
+                            );
+                            $statement = $connection->prepare('UPDATE slips SET image=:image WHERE slip_date=:slip_date AND user_id=:user_id');
+                            $statement->execute($params);
+                            
+                        } else {
+                            $params = array(
+                                'user_id' => $event['source']['userId'] ,
+                                'image' => $fileName,
+                                'slip_date' => date('Y-m-d'),
+                            );
+                            $statement = $connection->prepare('INSERT INTO slips (user_id, image, slip_date) VALUES (:user_id, :image, :slip_date)');
+                            $statement->execute($params);
+                        }
+                    }
+                    // Bot response 
+                    $respMessage = 'Your data has saved.';
+                    $replyToken = $event['replyToken'];
+                    $textMessageBuilder = new TextMessageBuilder($respMessage);
+                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                    
+                    break; 
+            }
+		}
+	}
 }
+echo "OK";
 /*
 // Get message from Line API
 $content = file_get_contents('php://input');
@@ -126,4 +207,35 @@ if (!is_null($events['events'])) {
 }
 
 echo "OK";
+
+
+
+<?php
+ 
+require_once('./vendor/autoload.php');
+
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+// Token
+$channel_token = 'JbkbMF0zqa9cDn91X8Vqhx0CMgD7haLJlO2V2bm8GpU4RZOZSvQHKw2stIMaFPN/Nthz2ZuAUdT7D3g2xUPcS4dvFtzF32s+C7zKtq+/hBR6VNIYXADNVgap6/7hMe46fUUW88Fm9JkRGbhdljSIvQdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'fce33b682da751c51e13169d81c9b7a8';
+
+// Create bot
+$httpClient = new CurlHTTPClient($channel_token);
+$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+
+// Database connection 
+$host = 'ec2-23-23-242-163.compute-1.amazonaws.com';
+$dbname = 'dfitqn78lbn0av';
+$user = 'gwuaimhybkhmyz';
+$pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
+$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
+$result = $connection->query("SELECT * FROM .appointments");
+if($result !== null) {
+    echo $result->rowCount();
+}
+*/
+
 
