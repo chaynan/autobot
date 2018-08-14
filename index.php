9e9d198240c964f625fc2cf4f7cd1b93acf1f843
chaynan <chaynan70@hotmail.com>
2018-08-14 11:35:30 +0700

    e

--- index.php
+++ index.php
@@ -12,29 +12,29 @@
 // $channel_token = 'JbkbMF0zqa9cDn91X8Vqhx0CMgD7haLJlO2V2bm8GpU4RZOZSvQHKw2stIMaFPN/Nthz2ZuAUdT7D3g2xUPcS4dvFtzF32s+C7zKtq+/hBR6VNIYXADNVgap6/7hMe46fUUW88Fm9JkRGbhdljSIvQdB04t89/1O/w1cDnyilFU=';
 // $channel_secret = 'fce33b682da751c51e13169d81c9b7a8';
 
-// // Get message from Line API
-// $content = file_get_contents('php://input');
-// $events = json_decode($content, true);
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
 
-if (!is_null($events['events'])) {
+// if (!is_null($events['events'])) {
 
-	// Loop through each event
-	foreach ($events['events'] as $event) {
+// 	// Loop through each event
+// 	foreach ($events['events'] as $event) {
     
-        // Line API send a lot of event type, we interested in message only.
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+//         // Line API send a lot of event type, we interested in message only.
+// 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
-            // Get replyToken
+//             // Get replyToken
          
-            $replyToken = $event['replyToken'];
+//             $replyToken = $event['replyToken'];
 
-            try {
-                // Check to see user already answer
-                $host = 'ec2-23-23-242-163.compute-1.amazonaws.com';
-                $dbname = 'dfitqn78lbn0av';
-                $user = 'gwuaimhybkhmyz';
-                $pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
-                $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
+//             try {
+//                 // Check to see user already answer
+//                 $host = 'ec2-23-23-242-163.compute-1.amazonaws.com';
+//                 $dbname = 'dfitqn78lbn0av';
+//                 $user = 'gwuaimhybkhmyz';
+//                 $pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
+//                 $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
 
                 if ( sizeof($request_array['events']) > 0 )
                 {
