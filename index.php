c279086b360a920a3cbceeea76840d1e24498f9b
chaynan <chaynan70@hotmail.com>
2018-08-14 11:27:00 +0700

    e

--- index.php
+++ index.php
@@ -116,3 +116,18 @@ if (!is_null($events['events'])) {
 }
 
 echo "OK";
+function send_reply_message($url, $post_header, $post_body)
+{
+ $ch = curl_init($url);
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+
+ return $result;
+}
+
+?>
