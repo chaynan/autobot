faaa5239cac8b4f1517c78ff88418a36c69b0f8d
chaynan <chaynan70@hotmail.com>
2018-08-14 13:14:44 +0700

    e

--- index.php
+++ index.php
@@ -1,137 +1,78 @@
 <?php
- $API_URL = 'https://api.line.me/v2/bot/message/reply';
- $ACCESS_TOKEN = 'JbkbMF0zqa9cDn91X8Vqhx0CMgD7haLJlO2V2bm8GpU4RZOZSvQHKw2stIMaFPN/Nthz2ZuAUdT7D3g2xUPcS4dvFtzF32s+C7zKtq+/hBR6VNIYXADNVgap6/7hMe46fUUW88Fm9JkRGbhdljSIvQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
- $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+ 
+require_once('./vendor/autoload.php');
 
-// // require_once('./vendor/autoload.php');
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
-// use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-// use \LINE\LINEBot;
-// use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+$channel_token = 'JbkbMF0zqa9cDn91X8Vqhx0CMgD7haLJlO2V2bm8GpU4RZOZSvQHKw2stIMaFPN/Nthz2ZuAUdT7D3g2xUPcS4dvFtzF32s+C7zKtq+/hBR6VNIYXADNVgap6/7hMe46fUUW88Fm9JkRGbhdljSIvQdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'fce33b682da751c51e13169d81c9b7a8';
 
-// $channel_token = 'JbkbMF0zqa9cDn91X8Vqhx0CMgD7haLJlO2V2bm8GpU4RZOZSvQHKw2stIMaFPN/Nthz2ZuAUdT7D3g2xUPcS4dvFtzF32s+C7zKtq+/hBR6VNIYXADNVgap6/7hMe46fUUW88Fm9JkRGbhdljSIvQdB04t89/1O/w1cDnyilFU=';
-// $channel_secret = 'fce33b682da751c51e13169d81c9b7a8';
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
 
-// // Get message from Line API
-// $content = file_get_contents('php://input');
-// $events = json_decode($content, true);
+if (!is_null($events['events'])) {
 
-// if (!is_null($events['events'])) {
-
-// 	// Loop through each event
-// 	foreach ($events['events'] as $event) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
     
-//         // Line API send a lot of event type, we interested in message only.
-// 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+        // Line API send a lot of event type, we interested in message only.
+		if ($event['type'] == 'message' ) {
 
-//             // Get replyToken
-         
-//             $replyToken = $event['replyToken'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
 
-//             try {
-//                 // Check to see user already answer
-//                 $host = 'ec2-23-23-242-163.compute-1.amazonaws.com';
-//                 $dbname = 'dfitqn78lbn0av';
-//                 $user = 'gwuaimhybkhmyz';
-//                 $pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
-//                 $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
-                
-                    // switch($event['message']['type']){
-                    //     case 'text':
-                    //     $data_user = $event['message']['text'];
-                    //     $respMessage = $data_user;
+            try {
+                // Check to see user already answer
+                $host = 'ec2-23-23-242-163.compute-1.amazonaws.com';
+                $dbname = 'dfitqn78lbn0av';
+                $user = 'gwuaimhybkhmyz';
+                $pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
+                $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
+    
+                    switch($event['message']['type']){
+                        case 'text':
+                        $data_user = $event['message']['text'];
+                        $respMessage = $data_user;
                         
-                    //     $params = array(
-                    //                 'userID' => $event['source']['userId'],
-                    //                 'content' => $event['message']['text'],
-                    //             );
-                    //         $statement = $connection->prepare('INSERT INTO appointments ( user_id,content ) VALUES ( :userID, :content )');
-                    //         $statement->execute($params);   
+                        $params = array(
+                                    'userID' => $event['source']['userId'],
+                                    'answer' => $event['message']['text'],
+                                );
+                            $statement = $connection->prepare('INSERT INTO poll ( user_id, answer ) VALUES ( :userID, :answer )');
+                            $statement->execute($params);   
 
-                    //     break;
+                        break;
                         
-                    //     case 'image':
-                    //         $messageID =$event['message']['id'];
-                    //         $respMessage = $messageID; 
+                        case 'image':
+                            $messageID =$event['message']['id'];
+                            $respMessage = $messageID; 
                    
     
-                    //     break;
+                        break;
 
-                    //     default: 
-                    //         $respMessage = 'This is Default'; 
+                        default: 
+                            $respMessage = 'This is Default'; 
 
-                    //         break;
-                    // }
+                            break;
+                    }
     
-                // $httpClient = new CurlHTTPClient($channel_token);
-                // $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+
+                $httpClient = new CurlHTTPClient($channel_token);
+                $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
     
-                // $textMessageBuilder = new TextMessageBuilder($respMessage);
-                // $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                $textMessageBuilder = new TextMessageBuilder($respMessage);
+                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 
             
-            // } catch(Exception $e) {
-            //     error_log($e->getMessage());
-            // }
+            } catch(Exception $e) {
+                error_log($e->getMessage());
+            }
 
-		// }
-// 	}
-// }
-
-$request = file_get_contents('php://input');   // Get request content
-$request_array = json_decode($request, true);   // Decode JSON to Array
-
-if ( sizeof($request_array['events']) > 0 )
-{
-
- foreach ($request_array['events'] as $event)
- {
-  $reply_message = '';
-  $reply_token = $event['replyToken'];
-
-  if ( $event['type'] == 'message' ) 
-  {
-   if( $event['message']['type'] == 'text' )
-   {
-    $text = $event['message']['text'];
-    $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
-   }
-   else
-    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
-  
-  }
-  else
-   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
- 
-  if( strlen($reply_message) > 0 )
-  {
-   //$reply_message = iconv("tis-620","utf-8",$reply_message);
-   $data = [
-    'replyToken' => $reply_token,
-    'messages' => [['type' => 'text', 'text' => $reply_message]]
-   ];
-   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
-   echo "Result: ".$send_result."\r\n";
-  }
- }
+		}
+	}
 }
-  
+
 echo "OK";
-
-function send_reply_message($url, $post_header, $post_body)
-{
- $ch = curl_init($url);
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
- curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
- curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
- $result = curl_exec($ch);
- curl_close($ch);
-
- return $result;
-}
-
-?>
