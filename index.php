a8ad30b8f6ac2cf5b5e331d18db7b945f0a00b43
chaynan <chaynan70@hotmail.com>
2018-09-18 13:24:39 +0700

    3

--- index.php
+++ index.php
@@ -31,6 +31,8 @@ if (!is_null($events['events'])) {
                 $user = 'gwuaimhybkhmyz';
                 $pass = 'cb37b0b2797f5e53a4eb419c7fdabbd347a988bb3f5cec004ba794a2d71f8b7e';
                 $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
+                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
                 if($event['message']['type']=='text'){
                    $text = $event['message']['text'];
@@ -38,10 +40,19 @@ if (!is_null($events['events'])) {
 
                    $texttest1 = stripos($text , $test);
                    if($texttest1 !== false){
-                    $sql = sprintf("SELECT result FROM test WHERE key='$test' ");
-                    $result = $connection->query($sql);
+                    $stmt = $connection->prepare("SELECT result FROM test WHERE key='$test'"); 
+                    $stmt->execute();
+
+                    // $sql = sprintf("SELECT result FROM test WHERE key='$test' ");
+                    // $result = $connection->query($sql);
+
+                    // set the resulting array to associative
+                    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+                    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+                        $respMessage = $v;
+                    }
                             
-                        $respMessage =$result;
+                        // $respMessage =$result;
                    }else{
                         $respMessage = 'not found.';
                    }
