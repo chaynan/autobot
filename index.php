2ccc171fcafb53238a1448b6f6a0916c47b6093a
chaynan <chaynan70@hotmail.com>
2018-09-20 13:32:41 +0700

    1

--- index.php
+++ index.php
@@ -90,21 +90,18 @@ if (!is_null($events['events'])) {
                         $respMessage = $row['result'];
                         }
                     }else{
-                        $respMessage = "ไม่พบข้อมูล";
+                        $data = $connection->query("SELECT result FROM test WHERE key LIKE '%$text%' LIMIT 1")->fetchAll();
+                        if($data){
+                            foreach ($data as $row) { 
+                            $respMessage = $row['result'];
                         }
-
-
-
-
+                    }else{
+                        $respMessage = "ไม่พบข้อมูล";
                     }
-                    
-                    
-                    
-                                     
-
-                }          
-                }
-    
+                    }                                      
+                    }          
+                    }
+                    }
                     // switch($event['message']['type']){
                     //     case 'text':
  
