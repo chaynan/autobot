2caa1b5275d1bfecf7899e73ff5fe1642af63733
chaynan <chaynan70@hotmail.com>
2018-09-20 13:34:57 +0700

    1

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if (!is_null($events['events'])) {
                         $respMessage = $row['result'];
                         }
                     }else{
-                        $data = $connection->query("SELECT result FROM test WHERE key LIKE '%$text%' LIMIT 1")->fetchAll();
+                        $data = $connection->query("SELECT result FROM test WHERE key LIKE '%$text' LIMIT 1")->fetchAll();
                         if($data){
                             foreach ($data as $row) { 
                             $respMessage = $row['result'];
