9c915d3e085611c11f043d99e57712f2244993e6
chaynan <chaynan70@hotmail.com>
2018-09-27 10:29:09 +0700

    a

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if (!is_null($events['events'])) {
                                 if($result){
                                     $respMessage = 'อัพเดทแล้ว';
                                 }else{
-                                    $respMessage = 'อัพเดทข้อผิดพลาด1';
+                                    $respMessage = 'อัพเดทข้อผิดพลาด';
                                 }
                                 
                             }else{
@@ -70,7 +70,7 @@ if (!is_null($events['events'])) {
                                 if($result){
                                     $respMessage = 'บันทึกแล้ว';
                                 }else{
-                                    $respMessage = 'บันทึกข้อผิดพลาด2';
+                                    $respMessage = 'บันทึกข้อผิดพลาด';
                                 }
                             }
                  
@@ -90,6 +90,8 @@ if (!is_null($events['events'])) {
                             foreach ($data as $row) {
                             $respMessage = $row['result'];
                             }
+                        }else{
+                            $respMessage = "ไม่พบข้อมูล";
                         }
                     }
                 }
@@ -107,10 +109,49 @@ if (!is_null($events['events'])) {
 
 		}
 	}
+}switch ($typeMessage){
+case (preg_match('/[image|audio|video]/',$typeMessage) ? true : false) :
+$response = $bot->getMessageContent($idMessage);
+if ($response->isSucceeded()) {
+    // คำสั่ง getRawBody() ในกรณีนี้ จะได้ข้อมูลส่งกลับมาเป็น binary 
+    // เราสามารถเอาข้อมูลไปบันทึกเป็นไฟล์ได้
+    $dataBinary = $response->getRawBody(); // return binary
+    // ดึงข้อมูลประเภทของไฟล์ จาก header
+    $fileType = $response->getHeader('Content-Type');    
+    switch ($fileType){
+        case (preg_match('/^image/',$fileType) ? true : false):
+            list($typeFile,$ext) = explode("/",$fileType);
+            $ext = ($ext=='jpeg' || $ext=='jpg')?"jpg":$ext;
+            $fileNameSave = time().".".$ext;
+            break;
+        case (preg_match('/^audio/',$fileType) ? true : false):
+            list($typeFile,$ext) = explode("/",$fileType);
+            $fileNameSave = time().".".$ext;                        
+            break;
+        case (preg_match('/^video/',$fileType) ? true : false):
+            list($typeFile,$ext) = explode("/",$fileType);
+            $fileNameSave = time().".".$ext;                                
+            break;                                                      
+    }
+$botDataFolder = 'botdata/'; // โฟลเดอร์หลักที่จะบันทึกไฟล์
+$botDataUserFolder = $botDataFolder.$userID; // มีโฟลเดอร์ด้านในเป็น userId อีกขั้น
+if(!file_exists($botDataUserFolder)) { // ตรวจสอบถ้ายังไม่มีให้สร้างโฟลเดอร์ userId
+    mkdir($botDataUserFolder, 0777, true);
+}   
+// กำหนด path ของไฟล์ที่จะบันทึก
+$fileFullSavePath = $botDataUserFolder.'/'.$fileNameSave;
+file_put_contents($fileFullSavePath,$dataBinary); // ทำการบันทึกไฟล์
+$textReplyMessage = "บันทึกไฟล์เรียบร้อยแล้ว $fileNameSave";
+$replyData = new TextMessageBuilder($textReplyMessage);
+break;
 }
-
+$failMessage = json_encode($idMessage.' '.$response->getHTTPStatus() . ' ' . $response->getRawBody());
+$replyData = new TextMessageBuilder($failMessage);  
+break;                                                      
+default:
+$textReplyMessage = json_encode($events);
+$replyData = new TextMessageBuilder($textReplyMessage);         
+break;  
+}
 echo "OK";
 ?>
-<!-- else{
-    $respMessage = "ไม่พบข้อมูล";
-    } -->
