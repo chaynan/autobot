7f9e1b9ec70dd7ce117f86c32acd30eecb8a8771
chaynan <chaynan70@hotmail.com>
2018-09-28 16:19:24 +0700

    1

--- index.php
+++ index.php
@@ -4,8 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
  
-// include composer autoload
-require_once '../vendor/autoload.php';
+
  
 // การตั้งเกี่ยวกับ bot
 require_once 'bot_settings.php';
