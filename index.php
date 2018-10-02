263a0436b18095c5ec80ff7a835f130fc20755b0
chaynan <chaynan70@hotmail.com>
2018-10-02 13:27:54 +0700

    a

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ if (!is_null($events['events'])) {
                     'user-id' => $event['source']['userId'] ,
                     'image' => $fileName,
                 );
-                $statement = $connection->prepare('INSERT INTO id (id , image_id) VALUES (:user-id, :image');
+                $statement = $connection->prepare('INSERT INTO id (id , image_id) VALUES (:user-id, :image)');
                 $statement->execute($params);
                 if($statement){
                     $respMessage = 'บันทึกรูปแล้ว';
